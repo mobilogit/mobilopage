be15538f2303c56141ab026d73dd7491992ee70c
mobilogit <153889024+mobilogit@users.noreply.github.com>
2024-02-29 20:20:32 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Demo Flask App (CC-GitActions)
+## Purpose
+- commit code to development
+- run action
+  - build
+  - test
+  - deploy to Azure dev appliacation
+- pull request to production
+  - run action
+    - build
+    - deploy to Azure prd application   
---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, url_for, request, redirect
+
+# Use name of current module (__name__) as argument.
+app = Flask(__name__)
+
+# function defining a dynamically added routing entry
+def dynamical_routing_test():
+   return f'Dynamically added route<br><a href={ url_for("hello_world") }>take me home</a>'
+
+# adding dynamically a new route
+app.add_url_rule('/dynamic', 'dynamic_route', dynamical_routing_test)
+
+# home --> form to login
+@app.route('/')
+def hello_world():
+    VERSION = 40
+    content = f'''
+      <html>
+        <body>     
+          <h1>{ VERSION }</h1>
+          <form action = { url_for("login") } method = "post">
+            <p>Enter Name:</p>
+            <p><input type = "text" name = "user_name" /></p>
+            <p><input type = "submit" value = "submit" /></p>
+          </form>     
+        <p><a href={ url_for("dynamic_route") }>dynamic link</a></p>
+        </body>
+      </html>
+'''
+    return content
+
+# login action --> redirect to success
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['user_name']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('name')
+        return redirect(url_for('success', name=user))
+
+# show success and allow to go to home
+@app.route('/success/<name>')
+def success(name):
+    return f'Welcome {name} <p><a href={ url_for("hello_world") }>take me home</a></p>'
+
+
+
+if __name__ == '__main__':
+	app.run()
