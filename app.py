9b52029806a78df5305289acdcc78cc12c360fde
mobilogit <153889024+mobilogit@users.noreply.github.com>
2024-03-01 20:41:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.add_url_rule('/dynamic', 'dynamic_route', dynamical_routing_test)
 # home --> form to login
 @app.route('/')
 def hello_world():
-    VERSION = 40
+    VERSION = 41
     content = f'''
       <html>
         <body>     
